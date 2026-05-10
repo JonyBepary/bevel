e7daf9fb4f0c235ea24d314f387a54361d6916c8
JonyBepary <sohelahmedjony@gmail.com>
2026-05-10 21:21:35 +0600

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: JonyBepary <sohelahmedjony@gmail.com>

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 # Path to k8s config file
-export KUBECONFIG=/home/bevel/build/config
+export KUBECONFIG=/home/jony/blockchain_pract/project/bevel/build/config
 
 echo "Validatin network yaml"
-ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
+# ajv validate -s /home/jony/blockchain_pract/project/bevel/platforms/network-schema.json -d /home/jony/blockchain_pract/project/bevel/build/network.yaml 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/jony/blockchain_pract/project/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/jony/blockchain_pract/project/bevel/platforms/shared/inventory/ -e "@/home/jony/blockchain_pract/project/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
